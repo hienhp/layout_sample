f6b451801983f531cda7df58b9136626b597f324
htv-hienhp <hienhp@hybrid-technologies.co.jp>
2021-03-26 13:14:07 +0700

    Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', 'prettier/vue', 'prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
+  plugins: ['prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'vue/no-v-html': 'off',
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single', { 'avoidEscape': true }]
+  }
+}
